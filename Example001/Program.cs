4c674865c861c6278e00ec915e4149cb6e0748dc
Nata <muravevantsh@mail.ru>
2022-08-26 02:27:32 +0300

    Exaple001

---
+++ Example001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+void PrintArray (double [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j=0; j <matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (double [,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j =0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().NextDouble();
+        }
+    } 
+}
+int m = 5;
+int n = 6;
+double[,]matrix = new double [m,n];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
